c9d960863174f986cd969b9401ec1a07055d4878
Fedor Krutakov <fedor.krutakov@akbars.ru>
2021-03-06 16:34:59 +0300

    commit

--- deploy/redeploy.sh
+++ deploy/redeploy.sh
@@ -1,4 +1,4 @@
 mkdir tmp
-cp deploy.yaml tmp
-sed -i "s%image: fkrutakov/asphelloworld%image: fkrutakov/asphelloworld:$1%g" ./tmp/deploy.yaml
-microk8s kubectl apply -f ./tmp/deploy.yaml
+cp deploy.yaml deploy-tmp.yaml
+sed -i "s%image: fkrutakov/asphelloworld%image: fkrutakov/asphelloworld:$1%g" deploy-tmp.yaml
+microk8s kubectl apply -f deploy-tmp.yaml
