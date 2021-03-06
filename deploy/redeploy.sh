38c9d9090a871f45122c96af8fceacd7b1d9b257
Fedor Krutakov <fedor.krutakov@akbars.ru>
2021-03-06 17:23:21 +0300

    commit

--- deploy/redeploy.sh
+++ deploy/redeploy.sh
@@ -1,4 +1,4 @@
 cp deploy.yaml deploy-tmp.yaml
-export HELLO_WORLD_IMAGE=fkrutakov/asphelloworld:$1
+HELLO_WORLD_IMAGE="fkrutakov/asphelloworld:$1"
 envsubst < deploy-tmp.yaml
 microk8s kubectl apply -f deploy-tmp.yaml
