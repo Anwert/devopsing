94493143ab1cbc19b7addaf22e5e205214e42876
Fedor Krutakov <fedor.krutakov@akbars.ru>
2021-03-23 20:00:23 +0300

    commit

--- HelloWorld/Controllers/WeatherForecastController.cs
+++ HelloWorld/Controllers/WeatherForecastController.cs
@@ -36,7 +36,7 @@ namespace HelloWorld.Controllers
             .ToArray();
         }
         
-        [HttpGet("test125")]
+        [HttpGet("test1256")]
         public IActionResult GetTest1()
         {
             return Ok(new
